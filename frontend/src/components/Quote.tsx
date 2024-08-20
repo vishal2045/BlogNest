e8618463a72a0b1746f492731251f7ff3b499d6e
vishal2045 <63170247+vishal2045@users.noreply.github.com>
2024-08-20 13:56:14 +0530

    Update Quote.tsx

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,23 +1,20 @@
-const Quotes = () => {
-    return (
-      <div className="bg-slate-200 h-screen flex justify-center flex-col">
-        <div className="flex justify-center ">
-          <div className="max-w-lg">
-            <div className="text-3xl font-bold">
-              The customer service I recieved was exceptional. The support team went above and 
-              beyond to address my concerns.
-            </div>
-            <div className="flex justify max-w-md text-xl font-bold mt-4">
-              Jules Winfield
-            </div>
-            <div className="flex justify max-w-md text-sm font-medium text-slate-700">
-              CEO | Acme
-            </div>
-          </div>
-        </div>
+export const Quote = () =>{
+    return <div className=" bg-slate-200 h-screen flex justify-center flex-col ">
+        <div className="flex justify-center">
+            <div className=" max-w-lg">
+                <div className="text-2xl font-bold">
+                "The customer support i received was exceptional. The support team went above and
+                beyond to address my concerns"
+                </div>
+
+                <div className=" max-w-md text-xl font-semibold mt-4">
+                    Julies Winfield
+                </div>
         
-      </div>
-    )
-  }
-  
-  export default Quotes
+                <div className=" max-w-md text-sm font-light text-slate-400">
+                    CEO | Acme corp
+                </div>
+            </div>
+        </div>
+    </div>
+}
