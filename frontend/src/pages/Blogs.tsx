1ac8913d11f89593adf8faa9683f445aefa694e7
vishal2045 <63170247+vishal2045@users.noreply.github.com>
2024-08-20 12:41:22 +0530

    Update Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,49 +1,43 @@
-import { Appbar } from "../components/Appbar"
+import { AppBar } from "../components/AppBar"
 import { BlogCard } from "../components/BlogCard"
 import { BlogSkeleton } from "../components/BlogSkeleton";
 import { useBlogs } from "../hooks"
 
 export const Blogs = () => {
-
-  const {loading, blogs} = useBlogs();
-
-    if (loading){
-      return <div>
-        <Appbar/>
+  const {loading , blogs} = useBlogs() ;
+  if(loading){
+    return (
+      <div>
+        <AppBar />
         <div className="flex justify-center">
           <div>
-          <BlogSkeleton />
-          <BlogSkeleton />
-          <BlogSkeleton />
-          <BlogSkeleton />
-          <BlogSkeleton />
-          <BlogSkeleton />
-          <BlogSkeleton />
-          <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
           </div>
-          
         </div>
-      </div>
-    }
-  
-
+    </div>
+    )
+  }
   return (
     <div>
-       <Appbar />
-      <div  className="flex justify-center">
-        <div className="">
-        {blogs.map((blog,index) => <BlogCard 
-        key={index}
-         id={blog?.id}
-         authorname ={blog?.author?.name || "Anonymous"}
-         title={blog?.title}
-         content={blog?.content}
-         publishedDate={"2nd Feb 2024"} />) }
-        
-       
-      </div>
+      <AppBar />
+      <div className="flex justify-center">
+        <div>
+
+          {blogs.map(blog =>(
+            <BlogCard 
+            authorName ={blog.author.name || "Anonymous"}
+            title={blog.title}
+            content={blog.content}
+            id= {blog.id}
+            publishedDate="2nd Feb 2024"
+          />
+          ))}   
+        </div>
       </div>
     </div>
-    
   )
 }
