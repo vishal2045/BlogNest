512a7d554c71f391c737037b502f9e66ba099dee
vishal2045 <63170247+vishal2045@users.noreply.github.com>
2024-08-20 12:43:56 +0530

    Update Blog.tsx

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,29 +1,26 @@
-import { Appbar } from "../components/Appbar";
+import { useParams } from "react-router-dom";
+import { useBlog } from "../hooks"
 import { FullBlog } from "../components/FullBlog";
 import { Spinner } from "../components/Spinner";
-import { useBlog } from "../hooks";
-import {useParams} from "react-router-dom";
+import { AppBar } from "../components/AppBar";
 
-// atomFamilies/selectorFamilies
 export const Blog = () => {
-    const { id } = useParams();
-    const {loading, blog} = useBlog({
-        id: id || ""
-    });
-
-    if (loading || !blog) {
-        return <div>
-            <Appbar />
-        
-            <div className="h-screen flex flex-col justify-center">
-                
-                <div className="flex justify-center">
-                    <Spinner />
-                </div>
-            </div>
+  const {id} = useParams() ;
+  const {blog , loading} = useBlog({
+    id : id || ""
+  }) ;
+    if(loading || !blog){
+      return (
+      <div>
+        <AppBar />
+        <div className="h-screen flex flex-col justify-center items-center">
+            <Spinner />
         </div>
+      </div>
+      )
     }
+
     return <div>
-        <FullBlog blog={blog} />
+      <FullBlog blog = {blog} />
     </div>
 }
